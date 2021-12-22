f27cf4e1c7efbf46be44c8d50b7fa7b148e20462
P V Tejeswar Panda <pvtezeswarpanda8@gmail.com>
2021-12-22 17:26:14 +0000

    solved set problem.

--- STL/map_STL.cpp
+++ STL/map_STL.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void solve(){
+    int n;
+    cin >> n;
+    set<string> names;
+    string tmp;
+    while(n--){
+        cin >> tmp;
+        names.insert(tmp);
+    }
+    for(string s: names)
+        cout << s << endl;
+
+}
+
 int main(){
-    cout << "Hello\n";
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
 }
